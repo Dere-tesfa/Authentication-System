b78befdc451550dfa95a1574e19d91ff6421ac75
Dereje Tesfaye <deretesfa6@gmail.com>
2025-10-01 21:28:46 +0300

    add proxy with communicate front end and backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // proxy all /api requests to your Express backend
+      "/api": "http://localhost:8080"
+    }
+  }
+});
