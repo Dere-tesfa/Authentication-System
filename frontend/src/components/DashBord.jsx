f0757fed4407d72cf7570a34820ffb4db6565472
Dereje Tesfaye <deretesfa6@gmail.com>
2025-10-08 20:03:25 +0300

    dashboard

--- frontend/src/components/DashBord.jsx
+++ frontend/src/components/DashBord.jsx
@@ -2,7 +2,8 @@
 export default function Dashboard() {
     return (
         <div style={{ padding: "50px", textAlign: "center" }}>
-            <h1>Welcome to your Dashboard!</h1>
+            <h1>Hi there ✋</h1>
+            <h1>Welcome to your club Dashboard!</h1>
             <p>You are logged in successfully.</p>
         </div>
     );
